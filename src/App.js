34c5f47a91407730af75772236a60ad945178889
josemartin83 <josemartin83@gmail.com>
2021-04-21 10:44:17 +1200

    save app.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,10 @@ import Login from "./Components/LoginandRegister/Login";
 import Registerform from "./Components/LoginandRegister/RegisterForm";
 import ForgetPass from "./Components/LoginandRegister/ForgetPass";
 import Home from "./Components/LoginandRegister/Home";
-<<<<<<< HEAD
 import React from 'react';
 export default App;
-=======
 import ProfilePage from "./Components/Profile/ProfilePage";
 import WorkReadyPassportHandler from "./Components/WorkReadyPassportPDF/WorkReadyPassportHandler";
->>>>>>> f06cba59b362202899f57c9808a37d856ce3758e
 
 function App() {
   const [token, setToken] = React.useState('');
@@ -22,25 +19,6 @@ function App() {
  }
   return (
     <Router>
-<<<<<<< HEAD
-      <div>
-        {/* <MainNavigation /> */}
-        <main>
-          <Switch>
-            <Route path="/" exact>
-              <Home />
-            </Route>
-            <Route path="/login" component={Login} />
-            {/* <Route path="/home" component={Home}/> */}
-            <Route path="/register" component={Registerform} />
-            <Route path="/forgpass" component={ForgetPass} />
-            <Route path="/LoginPage" component={LoginPage} />
-            <Redirect to="/" />
-          </Switch>
-        </main>
-      </div>
-    </Router>
-=======
     <div>
            {/* <MainNavigation /> */}
              <main>
@@ -57,7 +35,6 @@ function App() {
              </main>
      </div>
      </Router>
->>>>>>> f06cba59b362202899f57c9808a37d856ce3758e
 
 
 
