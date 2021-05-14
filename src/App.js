b9acc0a45fd19511aef0455fa9b198907e64bba0
spijkemax <spijkemax@ecs.vuw.ac.nz>
2021-05-14 13:27:54 +1200

    App.js update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import React from 'react';
-import { Route, Redirect, Switch, BrowserRouter as Router } from 'react-router-dom';
+import LoginPage from './Components/LoginandRegister/Login';
+import {Route, Redirect, Switch, BrowserRouter as Router} from 'react-router-dom';
 import Login from "./Components/LoginandRegister/Login";
 import Registerform from "./Components/LoginandRegister/RegisterForm";
 import ForgetPass from "./Components/LoginandRegister/ForgetPass";
 import Home from "./Components/LoginandRegister/Home";
 import ProfilePage from "./Components/Profile/ProfilePage";
 import WorkReadyPassportHandler from "./Components/WorkReadyPassportPDF/WorkReadyPassportHandler";
-import Logout from './Components/LoginandRegister/Logout';
+import JobListing from "./Components/JobListing/jobListing";
+import ProfileEmployer from "./Components/Profile/ProfileEmployer";
+import EmployerAddJob from "./Components/EmployerJobSubmission/employerAddJob"
 export default App;
 
 function App() {
@@ -18,13 +20,17 @@ function App() {
           <main>
             <Switch>
            
-              <Route path="/home" component={Home} />
-              <Route path="/register" component={Registerform} />
-              <Route path="/forgpass" component={ForgetPass} />
-              <Route path="/profile" component={ProfilePage} />
-              <Route path="/logout" component={Logout} />
-              <Route path="/kapitiworkreadypassport" component={WorkReadyPassportHandler} />
-              <Redirect to="/profile" />
+            <Route path="/login" component={Login}/>
+                 <Route path="/home" component={Home}/>
+                 <Route path="/register" component={Registerform}/>
+                 <Route path="/forgpass" component={ForgetPass}/>
+                 <Route path="/LoginPage" component={LoginPage}/>
+                 <Route path="/profile" component={ProfilePage} />
+                 <Route path="/passport" component={WorkReadyPassportHandler} />
+                 <Route path="/joblisting" component={JobListing} />
+                 <Route path="/employerprofile" component={ProfileEmployer} />
+                 <Route path="/addJobEmployer" component={EmployerAddJob} />
+                 <Redirect to="/home"/> 
             </Switch>
           </main>
         </div>
